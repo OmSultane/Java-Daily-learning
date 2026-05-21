a4ff41fe457e8dc8cd2998fb79d670269be7564b
Om Sultane <sultaneom256@gmail.com>
2026-05-21 16:15:58 +0530

    Added some methods about ArrayList with comments

--- CollectionFramework/list.java
+++ CollectionFramework/list.java
@@ -22,24 +22,30 @@ public class list{
         // Creating a collection using ArrayList implementation
         ArrayList<Integer> numbers = new ArrayList<>(1000);
        
+
+        // Adding individual elements
+        numbers.add(10); 
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(1, 15);  //to add number at specified index
+        // Adding another collection
+        Collection<Integer> moreNumbers = new ArrayList<>();
+        moreNumbers.add(40);
+        moreNumbers.add(50);
+
+        numbers.addAll(moreNumbers);     //added a collection to another collection/ArrayList
         
-    //     // Adding individual elements
-    //     numbers.add(10); 
-    //     numbers.add(20);
-    //     numbers.add(30);
+        // System.out.println(numbers.get(3));
 
-    //     // Adding another collection
-    //     Collection<Integer> moreNumbers = new ArrayList<>();
-    //     moreNumbers.add(40);
-    //     moreNumbers.add(50);
+        numbers.set(2,25);   //to replace element element at 2nd index
 
-    //     numbers.addAll(moreNumbers);
-    //     System.out.println(numbers.get(3));
-    //     System.out.println("After adding elements: " + numbers);
+        numbers.trimToSize();   //this trims the *capacity* of list according to elements
+
+        System.out.println("After adding elements: " + numbers);
     
-    //    numbers.ensureCapacity(100);
+       numbers.ensureCapacity(100);
 
-    //    System.out.println();
+       System.out.println();
     
     
     
