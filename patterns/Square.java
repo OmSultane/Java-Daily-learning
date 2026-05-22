07c9ecad94b8ab5f345493ed90ec4e69b31085bb
Om Sultane <sultaneom256@gmail.com>
2026-05-22 18:59:16 +0530

    Added the logic part

--- patterns/Square.java
+++ patterns/Square.java
@@ -14,14 +14,18 @@ public class Square {
             for(int j=0; j<=n; j++){
                 System.out.print(n);
             }
-            
+
             System.out.println();
         }
     }
 }
 
-
-
-//                ***
-//                ***
-//                ***
+/*          
+                          ***
+                          ***
+                          ***
+Logic:- 1.Simply run a loop to print the rows for that first determine how many rows are their
+        2.Determine how many stars to print in each column 
+        3.print * \ n
+        
+*/
