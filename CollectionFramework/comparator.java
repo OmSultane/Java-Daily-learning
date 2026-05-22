d99b2dd2fcfef770be16baf3721896078e842317
Om Sultane <sultaneom256@gmail.com>
2026-05-22 15:38:29 +0530

    Added some description and logic part for comparator in form of comments

--- CollectionFramework/comparator.java
+++ CollectionFramework/comparator.java
@@ -1,3 +1,12 @@
+/* we use comparator to sort the objects in increasing/decreasing order
+to use comparator we must specify a class that *implements Comparator<T>* interface
+and override the compare *(T obj, T obj)* method that takes two parameters for comparing
+the logic is simply written in compare method 
+if we want to sort in increasing order the value to be return must be -ve
+if we want to sort in increasing order the value to be return must be -ve
+summary if  [ increasing o1-o2 ]  |  if  [ decreasing o2-o1 ]   */
+
+
 package CollectionFramework;
 import java.util.*;
 
