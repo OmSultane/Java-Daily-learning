6f50be7ad72c2d46b9dff616448a259c546b58d8
Om Sultane <sultaneom256@gmail.com>
2026-06-13 11:39:55 +0530

    Initial commit

---
+++ DSA/patterns/RightAngleStar.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class RightAngleStar {
+
+    private static void print(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int t = 2;
+
+        for(int i=0; i<t; i++){ 
+            
+            int n = input.nextInt();
+            print(n);
+        }
+
+        
+    }
+}
