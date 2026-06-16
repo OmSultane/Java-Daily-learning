1bbe6ea399a9d0d63ef785140678257fd95a206e
Om Sultane <sultaneom256@gmail.com>
2026-06-16 13:52:33 +0530

    Added a maven project for injecting literal values example

---
+++ Spring_learning/Injecting-Literal-Values/src/main/java/example/Bus.java
@@ -0,0 +1,16 @@
+package example;
+
+public class Bus implements Vehicle {
+	
+	
+	private String brandName;
+	
+	public String getBrandName() {
+		return brandName;
+	}
+	
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+
+}
---
+++ Spring_learning/Injecting-Literal-Values/src/main/java/example/Car.java
@@ -0,0 +1,16 @@
+package example;
+
+public class Car implements Vehicle {
+	
+	private String brandName;
+	
+	public String getBrandName() {
+		return brandName;
+	}
+	
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+	
+
+}
