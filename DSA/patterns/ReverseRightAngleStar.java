b627afc301aa7cb0be813dc74521eebb1df87d7d
Om Sultane <sultaneom256@gmail.com>
2026-06-18 09:27:21 +0530

    Printing right angle in reverse with a special formula

---
+++ DSA/patterns/ReverseRightAngleStar.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class ReverseRightAngleStar {
+    private static void print(int n){
+        for(int i = 1; i <= n; i++){
+
+            for(int j = 1; j <= n-i+1; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int t = 2;
+
+        for(int i=0; i<t; i++){ 
+            
+            int n = input.nextInt();
+            print(n);
+        }
+
+        input.close();
+
+    }
+}
+
+
+/*
+
+o/p :
+1 2 3
+1 2
+1
+
+Logic : 
+in every row we are printing n-i+1 ....total no.or rows - curr row no. + 1
+so inner loop condition should be n-i+1
+
+*/
