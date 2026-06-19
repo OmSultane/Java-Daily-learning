a793e2d94115e949680287273800a854557f4968
Om Sultane <sultaneom256@gmail.com>
2026-06-19 18:52:43 +0530

    An optimal solution for the problem to count pairs whose sumis less than target

---
+++ DSA/CountPairsWhoseSumIsLessThanTarget.java
@@ -0,0 +1,25 @@
+package DSA;
+
+import java.util.List;
+
+public class CountPairsWhoseSumIsLessThanTarget {
+
+    public int countPairs(List<Integer> nums, int target) {
+        nums.sort((a,b) -> a-b);
+        int res = 0;
+
+        int l = 0;
+        int r = nums.size()-1;
+
+        while(l != r){
+            if(nums.get(l) + nums.get(r) < target){
+                res += r-l;
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return res;
+    }
+}
