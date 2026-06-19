10fcd8c3f3ee76c0d914269c2cf53920e0d2abf3
Om Sultane <sultaneom256@gmail.com>
2026-06-19 19:59:33 +0530

    Solution to remove the duplicates from sorted array

---
+++ DSA/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,17 @@
+package DSA;
+
+public class RemoveDuplicatesFromSortedArray {
+    
+    public int removeDuplicates(int[] nums) {
+        int k = 1;
+
+        for(int i = 1; i< nums.length; i++){
+            if(nums[i] != nums[i-1]){
+                k += 1;
+                nums[k-1] = nums[i];
+            }
+        }
+        return k;
+    }
+
+}
