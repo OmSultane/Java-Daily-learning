5873af8ded4643ddba1a732e316d760aac18b901
Om Sultane <sultaneom256@gmail.com>
2026-06-21 11:29:51 +0530

    Added the description and logic section of question using commnets

--- DSA/TwoSum167.java
+++ DSA/TwoSum167.java
@@ -17,3 +17,19 @@ class TwoSum167 {
         return null;
     }
 }
+
+/*
+        --------------Two Sum II - Input Array Is Sorted-------------
+Given an integer array numbers(already sorted).
+Find two numbers such that they add up to a specific target number.
+Array is 1-based index given so while returning indices we have to add 1 extra
+
+Logic:
+We can solve it using two pointer approach where i will be pointing 1st element and j pointing last
+The loop runs while i<j
+We'll check two conditions 
+if sum of i&j is greater than target -> decrement j
+if sum of i&j is less than target -> increment i
+else means i & j we've found the elements return indices by adding 1(1-based index array)
+
+*/
