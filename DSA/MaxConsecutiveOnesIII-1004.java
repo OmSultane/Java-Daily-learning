c614abc13e352a161dbb190c12ec827e39286ccd
Om Sultane <sultaneom256@gmail.com>
2026-06-25 22:52:00 +0530

    leetcode: 1004 problem

---
+++ DSA/MaxConsecutiveOnesIII-1004.java
@@ -0,0 +1,27 @@
+package DSA;
+
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int left = 0;
+        int zeroCount = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                zeroCount++;
+            }
+
+            // Shrink window if zeros exceed k
+            while (zeroCount > k) {
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
