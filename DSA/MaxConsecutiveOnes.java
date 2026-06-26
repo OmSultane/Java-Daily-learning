573deae61c2a9104a5d307ef80b691e325a7ee7e
Om Sultane <sultaneom256@gmail.com>
2026-06-26 21:41:35 +0530

    Leetcode:485 problem solving by sliding window technique

--- DSA/MaxConsecutiveOnes.java
+++ DSA/MaxConsecutiveOnes.java
@@ -1,22 +1,49 @@
+// package DSA;
+
+// public class MaxConsecutiveOnes {
+    
+//     public int findMaxConsecutiveOnes(int[] nums) {
+
+//         int count = 0;
+//         int max = 0;
+
+//         for (int num : nums) {
+
+//             if (num == 1) {
+//                 count++;
+//                 max = Math.max(max, count);
+//             } else {
+//                 count = 0;
+//             }
+//         }
+
+//         return max;
+//     }
+// }
+
+
 package DSA;
 
 public class MaxConsecutiveOnes {
     
     public int findMaxConsecutiveOnes(int[] nums) {
 
-        int count = 0;
-        int max = 0;
+        int left = 0;
+        int ans = -1;
+        int window = 0;
 
-        for (int num : nums) {
+        for(int right = 0; right<nums.length; right++){
+            window = window+nums[right];
 
-            if (num == 1) {
-                count++;
-                max = Math.max(max, count);
-            } else {
-                count = 0;
+            while(right-left+1 != window){
+                window -= nums[left];
+                left++;
             }
+
+            ans = Math.max(right-left+1, ans);
         }
 
-        return max;
+
+        return ans;
     }
 }
