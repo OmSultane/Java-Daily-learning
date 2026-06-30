fb7f8b1d2e82fc932f04e6e4871c7dba3add7191
Om Sultane <sultaneom256@gmail.com>
2026-06-30 16:05:06 +0530

    Leetcode: 643 problem

---
+++ DSA/MaximumAverageSubarrayI643.java
@@ -0,0 +1,24 @@
+package DSA;
+
+public class MaximumAverageSubarrayI643 {
+    public double findMaxAverage(int[] nums, int k) {
+        int n = nums.length;
+        int left = 0;
+        double ans = 0;
+        double window = 0;
+
+        for(int i = 0; i < k; i++){
+            window += nums[i];
+        }
+
+        ans = window/k;
+
+        for(int right = k; right < nums.length; right++){
+            window += nums[right] - nums[right - k];
+
+            ans = Math.max(ans, window/k);
+        }
+
+        return ans;
+    }
+}
