7203442f0a5a59e56d63684cd2fbc2fd5496e524
Om Sultane <sultaneom256@gmail.com>
2026-07-01 20:19:48 +0530

    Leetcode: 1456 problem - Maximum Number of Vowels in a Substring of Given Length

---
+++ DSA/MaxNoOfVowelsInSubs1456.java
@@ -0,0 +1,27 @@
+package DSA;
+
+public class MaxNoOfVowelsInSubs1456 {
+    
+    public int maxVowels(String s, int k) {
+        int l = 0, r = 0, window = 0, res = 0;
+
+        for(; r < k; r++){
+            window += isVowel(s.charAt(r));
+        }  
+        res = window;
+
+        while(r < s.length()){
+            window -= isVowel(s.charAt(l++));
+            window += isVowel(s.charAt(r++));
+            
+            res = Math.max(res, window);
+        }      
+
+        return res;
+
+    }
+
+    public int isVowel(char c){
+        return ( c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') ? 1 : 0; 
+    }
+}
