9180c3d493b656cf8883db1f865447105db55b81
Om Sultane <sultaneom256@gmail.com>
2026-07-02 19:58:56 +0530

    Leetcode: 2351 problem

---
+++ DSA/HashMap/FirstLetterToAppearTwice2351.java
@@ -0,0 +1,20 @@
+package DSA.HashMap;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FirstLetterToAppearTwice2351 {
+    public char repeatedCharacter(String s) {
+        Set<Character> set = new HashSet<>();
+
+        for(int i = 0; i < s.length(); i++){
+            if(set.contains(s.charAt(i))){
+                return s.charAt(i);
+            }
+            else{
+                set.add(s.charAt(i));
+            }
+        }
+        return '0';
+    }
+}
