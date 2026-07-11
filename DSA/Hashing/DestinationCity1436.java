5e05cded1266126cd22b265bfac4dee06105c159
Om Sultane <sultaneom256@gmail.com>
2026-07-11 21:18:08 +0530

    Leetcode: 1436 problem

---
+++ DSA/Hashing/DestinationCity1436.java
@@ -0,0 +1,22 @@
+package DSA.Hashing;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DestinationCity1436 {
+    public String destCity(List<List<String>> paths) {
+        Set<String> set = new HashSet();
+
+        for(List<String> path : paths){
+            set.add(path.get(0));
+        }
+
+        for(List<String> path : paths){
+            if(!set.contains(path.get(1))){
+                return path.get(1);
+            }
+        }
+        return null;
+    }
+}
