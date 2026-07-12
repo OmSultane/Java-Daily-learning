d21747fba92904b14cc4ead3b9e1a35768a725dd
Om Sultane <sultaneom256@gmail.com>
2026-07-12 08:49:43 +0530

    Leetcode: 1512 problem

---
+++ DSA/Arrays/NumberofGoodPairs1512.java
@@ -0,0 +1,23 @@
+package DSA.Arrays;
+
+public class NumberofGoodPairs1512{
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public static int numIdenticalPairs(int[] nums){
+        int res = 0;
+        int countArray[] = new int[101];
+
+        for(int num: nums){
+            countArray[num]++;
+        }
+
+        for(int count : countArray){
+            count = (count * (count - 1) / 2);
+            res += count;
+        }
+
+        return res;
+    }
+}
