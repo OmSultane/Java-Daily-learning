215aff839b29a7b51dba1d479e686c717583d778
Om Sultane <sultaneom256@gmail.com>
2026-07-12 08:52:17 +0530

    Leetcode: 1394 problem

---
+++ DSA/Arrays/FindLuckyIntegerInArray1394.java
@@ -0,0 +1,18 @@
+package DSA.Arrays;
+
+public class FindLuckyIntegerInArray1394 {
+    public int findLucky(int[] arr){
+        int[] ca = new int[501];
+        for(int i : arr){
+            ca[i]++;
+        }
+
+        for(int i = 500; i > 0; i--){
+            if(i == ca[i]){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
