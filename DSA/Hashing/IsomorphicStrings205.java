0728beea4126c341f6cd044d167be36f149414e7
Om Sultane <sultaneom256@gmail.com>
2026-07-13 23:24:58 +0530

    Leetcode: 205 problem

---
+++ DSA/Hashing/IsomorphicStrings205.java
@@ -0,0 +1,21 @@
+package DSA.Hashing;
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsomorphicStrings205 {
+    public boolean isIsomorphic(String s, String t){
+        Map<Character, Character> map = new HashMap();
+        for(int i = 0; i < s.length(); i++){
+            char ss = s.charAt(i);
+            char tt = t.charAt(i);
+            if((map.containsKey(ss) && map.get(ss) != tt ) ||
+                (!map.containsKey(ss) && map.values().contains(tt))
+            ){
+                return false;
+            }
+            map.put(ss, tt);
+        }
+
+        return true;
+    }
+}
