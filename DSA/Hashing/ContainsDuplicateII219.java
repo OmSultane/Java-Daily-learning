d06f106e147916d01a3e4a524867bed62edeb280
Om Sultane <sultaneom256@gmail.com>
2026-07-19 18:53:13 +0530

    Leetcode: 219 problem

---
+++ DSA/Hashing/ContainsDuplicateII219.java
@@ -0,0 +1,21 @@
+package DSA.Hashing;
+
+import java.util.Map;
+import java.util.HashMap;
+
+class ContainsDuplicateII219 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap();
+        int idx = 0;
+
+        for (int i : nums){
+            if(map.containsKey(i) && Math.abs(idx - map.get(i)) <= k){
+                return true;
+            }else{
+                map.put(i, idx);
+            }
+            idx++;
+        }
+        return false;
+    }
+}
