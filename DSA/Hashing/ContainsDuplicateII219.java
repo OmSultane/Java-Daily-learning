5a477a65736ff34a6e68c08c45cf645333148dc1
Om Sultane <sultaneom256@gmail.com>
2026-07-19 20:32:37 +0530

    Leetcode: 219 problem optimized approach using Set

--- DSA/Hashing/ContainsDuplicateII219.java
+++ DSA/Hashing/ContainsDuplicateII219.java
@@ -1,20 +1,36 @@
 package DSA.Hashing;
 
-import java.util.Map;
-import java.util.HashMap;
+// import java.util.Map;
+import java.util.Set;
+// import java.util.HashMap;
+import java.util.HashSet;
 
 class ContainsDuplicateII219 {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap();
-        int idx = 0;
+        // Map<Integer, Integer> map = new HashMap();
+        // int idx = 0;
 
-        for (int i : nums){
-            if(map.containsKey(i) && Math.abs(idx - map.get(i)) <= k){
+        // for (int i : nums){
+        //     if(map.containsKey(i) && Math.abs(idx - map.get(i)) <= k){
+        //         return true;
+        //     }else{
+        //         map.put(i, idx);
+        //     }
+        //     idx++;
+        // }
+        // return false;
+
+        Set<Integer> set = new HashSet();
+
+        for(int i = 0; i < nums.length; i++){
+            if(set.contains(nums[i])){
                 return true;
-            }else{
-                map.put(i, idx);
             }
-            idx++;
+            set.add(nums[i]);
+           
+            if(set.size() > k){
+                set.remove(nums[i - k]);
+            }
         }
         return false;
     }
