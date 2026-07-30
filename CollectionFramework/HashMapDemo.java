929554e885bd66b18fad1623e2fa0d60513ace2f
Om Sultane <sultaneom256@gmail.com>
2026-07-30 06:31:54 +0530

    HashMap keySet() & entrySet() methods with use case

--- CollectionFramework/HashMapDemo.java
+++ CollectionFramework/HashMapDemo.java
@@ -1,5 +1,7 @@
 package CollectionFramework;
+import java.util.Map;
 import java.util.HashMap;
+import java.util.Set;
 
 public class HashMapDemo {
     public static void main(String[] args) {
@@ -13,8 +15,23 @@ public class HashMapDemo {
         System.out.println(map);
 
         System.out.println(map.containsKey(3));
+        System.out.println(map.containsValue("om"));
+        
+        //to acess values we can loop on keys by using KeySet
+        Set<Integer> keys = map.keySet();
+        for(int i : keys){
+            System.out.println(map.get(i));
+        }
+
+        //we can also acess key & value using Map.Entry
+        Set<Map.Entry<Integer, String>> entries = map.entrySet();
+        
+        for(Map.Entry<Integer, String> entry : entries){
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            entry.setValue(entry.getValue().toUpperCase());
+        }
 
         
-
     }
 }
