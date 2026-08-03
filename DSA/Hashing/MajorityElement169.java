493402a957d399d6ad4c1ae7cdeb323284446cf7
Om Sultane <sultaneom256@gmail.com>
2026-08-03 19:25:38 +0530

    Leetcode: 169 problem

---
+++ DSA/Hashing/MajorityElement169.java
@@ -0,0 +1,21 @@
+package DSA.Hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length / 2;
+        Map<Integer, Integer> map = new HashMap();
+
+        for(int i : nums){
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        for(int i : map.keySet()){
+            if(map.get(i) > n){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
