4de4f8db78cabf444a67d324e50f93f6f5882786
Om Sultane <sultaneom256@gmail.com>
2026-08-08 00:06:29 +0530

    Leetcode: 496 problem optimal solution

--- DSA/Arrays/NextGreaterElementI496.java
+++ DSA/Arrays/NextGreaterElementI496.java
@@ -1,28 +1,50 @@
 package DSA.Arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int[] res = new int[nums1.length];
         boolean found = false;
+        Map<Integer, Integer> map = new HashMap();
+        Stack<Integer> stack = new Stack();
 
+        for(int i = 0; i < nums2.length; i++){
+            while(!stack.isEmpty() && stack.peek() < nums2[i]){
+                map.put(stack.pop(), nums2[i]);
+            }
+            stack.push(nums2[i]);
+        }
+        for(int i : stack){
+            map.put(i, -1);
+        }
+        
         for(int i = 0; i < nums1.length; i++){
-            found = false;
-            for(int j = 0; j < nums2.length; j++){
-                if(nums1[i] == nums2[j]){
-                    found = true;
-                }
-                if(found && nums2[j] > nums1[i]){
-                    res[i] = nums2[j];
-                    break;
-                }
-            }
+            res[i] = map.get(nums1[i]);
         }
 
-        for(int i = 0; i < res.length; i++){
-            if(res[i] == 0){
-                res[i] = -1;
-            }
-        }
         return res;
+
+            // found = false;
+            // for(int j = 0; j < nums2.length; j++){
+            //     if(nums1[i] == nums2[j]){
+            //         found = true;
+            //     }
+            //     if(found && nums2[j] > nums1[i]){
+            //         res[i] = nums2[j];
+            //         break;
+            //     }
+        //     }
+        // }
+        
+        // for(int i = 0; i < res.length; i++){
+        //     if(res[i] == 0){
+        //         res[i] = -1;
+        //     }
+        // }
+        // return res;
     }
 }
