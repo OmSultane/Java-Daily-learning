6b53bc7cdc958e597a8ab358d00bc44382afed13
Om Sultane <sultaneom256@gmail.com>
2026-08-13 21:02:00 +0530

    Leetcode: 2390 problem slightly optimized solution

--- DSA/Stack/RemovingStarsFromString2390.java
+++ DSA/Stack/RemovingStarsFromString2390.java
@@ -2,15 +2,34 @@ package DSA.Stack;
 
 class Solution {
     public String removeStars(String s) {
-       StringBuilder sb = new StringBuilder();
+    //    StringBuilder sb = new StringBuilder();
 
-       for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == '*'){
-                sb.deleteCharAt(sb.length()-1);
+    //    for(int i = 0; i < s.length(); i++){
+    //         if(s.charAt(i) == '*'){
+    //             sb.deleteCharAt(sb.length()-1);
+    //         }else{
+    //             sb.append(s.charAt(i));
+    //         }
+    //     }
+    //     return sb.toString();
+
+        int i = 0;
+        char []arr = new char[s.length()];
+        for(int j = 0; j < s.length(); j++){
+            if(s.charAt(j) == '*'){
+                i--;
+
             }else{
-                sb.append(s.charAt(i));
+                arr[i] = s.charAt(j);
+                i++;
             }
         }
+
+        StringBuilder sb = new StringBuilder();
+        for(int k = 0; k < i; k++){
+            sb.append(arr[k]);
+        }
+
         return sb.toString();
     }
 }
