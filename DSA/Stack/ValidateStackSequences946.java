2f8ead4156771885975224f0d592803183d55f59
Om Sultane <sultaneom256@gmail.com>
2026-08-30 04:49:39 +0530

    Leetcode 946 problem

---
+++ DSA/Stack/ValidateStackSequences946.java
@@ -0,0 +1,19 @@
+package DSA.Stack;
+import java.util.Stack;
+
+class solution{
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int j = 0;
+        Stack<Integer> st = new Stack();
+
+        for(int i = 0; i < pushed.length; i++){
+            st.push(pushed[i]);
+            while(!st.isEmpty() && st.peek() == popped[j]){
+                st.pop();
+                j++;
+            }
+        }
+        
+        return st.isEmpty();
+    }
+}
