7ee72cedf9a68d71f78ccc81588d12efd03c5716
Om Sultane <sultaneom256@gmail.com>
2026-09-03 11:59:49 +0530

    Leetcode: 100 problem

---
+++ DSA/Tree/SameTree100.java
@@ -0,0 +1,36 @@
+package DSA.Tree;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode a, TreeNode b) {
+        if(a == null && b == null){
+            return true;
+        }
+        if(a == null || b == null){
+            return false;
+        }
+        if(a.val != b.val){
+            return false;
+        }
+
+        boolean lans = isSameTree(a.left, b.left);
+        boolean rans = isSameTree(a.right, b.right);
+
+        return lans && rans;
+
+    }
+}
